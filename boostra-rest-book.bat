07128724bca750c0c3ddd01e9436ace04a0e69e6
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-23 11:58:47 -0500

    feat:first commit

---
+++ boostra-rest-book.bat
@@ -0,0 +1,1 @@
+mvn -U io.quarkus:quarkus-maven-plugin:create -DprojectGroupId="org.agoncal.quarkus.microservices" -DprojectArtifactId="rest-book" -DclassName="org.agoncal.quarkus.microservices.book.BookResource" -Dpath="/api/books" -Dextensions="resteasy-jsonb, smallrye-openapi"
---
+++ bootstrap-rest-number.bat
@@ -0,0 +1,1 @@
+mvn -U io.quarkus:quarkus-maven-plugin:create -DprojectGroupId="org.agoncal.quarkus.microservices" -DprojectArtifactId="rest-number" -DclassName="org.agoncal.quarkus.microservices.number.NumberResource" -Dpath="/api/numbers" -Dextensions="resteasy-jsonb, smallrye-openapi"
