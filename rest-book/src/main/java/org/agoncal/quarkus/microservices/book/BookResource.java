7e2403b04e9836025c25c78e808ec17be6d01c4c
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-29 23:17:31 -0500

    feat: Invoking the Number Microservice

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -2,6 +2,7 @@ package org.agoncal.quarkus.microservices.book;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -17,6 +18,9 @@ public class BookResource {
     @Inject
     Logger logger;
 
+    @RestClient
+    NumberProxy proxy;
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -28,7 +32,7 @@ public class BookResource {
             @FormParam("year") int yearOfPublication,
             @FormParam("genre") String genre) {
         Book book = new Book();
-        book.isbn13 = "13-We will get it later from the number microservices";
+        book.isbn13 = proxy.generateIsbnNumbers().isbn13;
         book.title = title;
         book.author = author;
         book.yearOfPublication = yearOfPublication;
---
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/IsnbThirteen.java
@@ -0,0 +1,8 @@
+package org.agoncal.quarkus.microservices.book;
+
+import javax.json.bind.annotation.JsonbProperty;
+
+public class IsnbThirteen {
+    @JsonbProperty("isbn_13")
+    public String isbn13;
+}
