4948433e1322d2caa2ae62528b6d424b7007e501
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-29 23:43:20 -0500

    feat: dealing with communication failure

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,14 +1,19 @@
 package org.agoncal.quarkus.microservices.book;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.Instant;
 
 @Path("/api/books")
@@ -26,6 +31,8 @@ public class BookResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Operation(summary = "Create a Book",
             description = "Creates a Book with an ISBN number")
+    @Retry(maxRetries = 3, delay = 3000)
+    @Fallback(fallbackMethod = "fallingbackOnCreatingABook")
     public Response createABook(
             @FormParam("title") String title,
             @FormParam("author") String author,
@@ -43,5 +50,32 @@ public class BookResource {
                 .status(201)
                 .entity(book)
                 .build();
+    }
+
+    public Response fallingbackOnCreatingABook(
+            String title,
+            String author,
+            int yearOfPublication,
+            String genre) throws FileNotFoundException {
+        Book book = new Book();
+        book.isbn13 = "Will be set later";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        saveBookOnDisk(book);
+        logger.warn("Book saved on disk: " + book);
+        return Response
+                .status(206)
+                .entity(book)
+                .build();
+    }
+
+    private void saveBookOnDisk(Book book) throws FileNotFoundException {
+        String bookJson = JsonbBuilder.create().toJson(book);
+        try (PrintWriter out = new PrintWriter("book-" + Instant.now().toEpochMilli() + ".json")) {
+            out.println(bookJson);
+        }
     }
 }
